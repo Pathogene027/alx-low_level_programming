aae6d55cda6b4ba4a92873a09209f5e298af4ccd
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-17 15:53:05 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 /**
-* main - main is the main function of the program
-* Description: It is the main entry point of the program
-* printf:
-* return
+* main - run a text printf and leave a new line
+*
+* Return: 0
 */
 int main(void)
 {
-/**
-* printf - it is a function that prints the text
-* Description: It print out the text with proper grammar
-* @"with proper grammar, but the outcome is a piece of art,
-* @\n is a newline escape
-**/
-printf("with proper grammar, but the outcome is a piece of art,\n");
-/**
-* return - it is a function that pruns last in the program
-* Description: It allows no other code to run after it execute
-* @0 means the return should return 0
-**/
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 /**
-* main - The main function
-* Description: The main entry point to the other programs
-* char character
+* main - The function prints the sizes of all data types
+*
+* Return: 0
 */
 int main(void)
 {
-char character;
-int integer;
-long int longinteger;
-long long int longlonginteger;
-float decimal;
-printf("Size of a char: %d byte(s)\n", sizeof(character));
-printf("Size of an int : %d byte(s)\n", sizeof(integer));
-printf("Size of a long int: %d byte(s)\n", sizeof(longinteger));
-printf("Size of a long long int: %d byte(s)\n", sizeof(longlonginteger));
-printf("Size of a float: %d byte(s)", sizeof(decimal));
-/**
-* return - The function that ends a program
-* Description: It allows no other code from running after it runs
-*/
-return (0);
+	char character;
+	int integer;
+	long int longinteger;
+	long long int longlonginteger;
+	float decimal;
+
+	printf("Size of a char: %lu byte(s)\n",
+	(unsigned long)sizeof(character));
+	printf("Size of an int : %lu byte(s)\n",
+	(unsigned long)sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n",
+	(unsigned long)sizeof(longinteger));
+	printf("Size of a long long int: %lu byte(s)\n",
+	(unsigned long)sizeof(longlonginteger));
+	printf("Size of a float: %d byte(s)",
+	(unsigned long)sizeof(decimal));
+	return (0);
 }
