e9bba35573ec7341cea2c030c0911c1b05ba31e4
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-17 09:35:08 -0700

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - main is the main function of the program
+* Description: It is the main entry point of the program
+*/
+int main(void){
+/**
+* printf - it is a function that prints the text
+* Description: It print out the text with proper grammar
+**/
+printf("with proper grammar, but the outcome is a piece of art,");
+/**
+* return - it is a function that pruns last in the program
+* Description: It allows no other code to run after it execute
+**/
+return(0);
+
