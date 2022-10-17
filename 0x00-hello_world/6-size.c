a77397cab60b2b8d83ad5a1d4de61d30dcf0bc16
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-17 11:06:26 -0700

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int integer;
 long int longinteger;
 long long int longlonginteger;
 float decimal;
-printf("Size of a char: %c byte(s)\nSize of an int : %d byte(s)\n
-Size of a long int: %ld byte(s)\nSize of a long long int: %lld byte(s)\n
-Size of a float: %f byte(s)", sizeof(character),
-sizeof(integer), sizeof(longinteger),
-sizeof(longlonginteger), sizeof(decimal));
+printf("Size of a char: %c byte(s)\n", sizeof(character));
+printf("Size of an int : %d byte(s)\n", sizeof(integer));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longinteger));
+printf("Size of a long long int: %lld byte(s)\n", sizeof(longlonginteger));
+printf("Size of a float: %f byte(s)", sizeof(decimal));
 /**
 * return - The function that ends a program
 * Description: It allows no other code from running after it runs
