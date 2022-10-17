a8269ff08a18c802df385dbb93692cfda2408e05
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-17 13:58:27 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 /**
 * main - A function that serves as the main entry point for the c program
-* Description: A main gateway to the program itself
+* return: 0
 */
 int main(void)
 {
-/**
-* puts - A c function that prints a text and give it a new line
-* Description: It allows text to be printed out using stdout
-*/
 puts("\"Programming is like building a multilingual puzzle");
-/**
-* return - A function that ends the program
-* Description: It allows no other program to run after it is run
-*/
 return (0);
 }
