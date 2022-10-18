49995f7b0cdf83059aa7484a761eb97ba3d98b88
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-18 09:42:18 -0700

    new

---
+++ 0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - this function prints a text _putchar
+*
+* Return: this value returns 0
+int main (void)
+*/
+{
+	puts("_putchar");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# alx-low_level_programming
+This is an ALX project focused on utilizing the C language
+for programming sophisticated solutions.
