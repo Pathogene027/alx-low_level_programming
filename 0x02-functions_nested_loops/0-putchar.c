d8f28eb0e97697cb425808e1c8edce2fc4bd1cd5
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-18 15:34:40 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,19 @@
 *
 * Return: returns 0
 */
+void print_alphabet(void)
+{
+	char ch[100]="I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game";
+	int i=0;
+	while (ch[i]!='\0')
+	{
+		putchar(tolower(ch[i]));
+		i++;
+		return;
+	}
+}
 int main(void)
 {
-	puts("_putchar");
+	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@
 * Return: returns an integer
 */
 int putchar(int n);
+void print_alphabet(void);
