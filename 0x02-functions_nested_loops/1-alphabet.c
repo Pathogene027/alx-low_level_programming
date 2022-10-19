66b7aa7c3225e03edd9b784ce642b1a46312e069
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-18 18:19:18 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "main.h"
 /**
 * print_alphabet - print a group of text
@@ -8,7 +9,7 @@
 */
 void print_alphabet(void)
 {
-	char ch[100] = "I sometimes suffer from insomnia. And when I "
+	char ch[100] = "I sometimes suffer from insomnia. And when I ";
 	char ch1[50] = "can't fall asleep, I play what I call the alphabet game";
 	int i = 0;
 
@@ -20,7 +21,7 @@ void print_alphabet(void)
 		i++;
 	};
 	putchar('\n');
-};
+}
 /**
 * main - main entry point of the program
 *
@@ -30,4 +31,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-};
+}
