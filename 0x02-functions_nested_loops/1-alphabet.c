ca981e62ba5167075f4938bcd017e753eac91697
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-18 18:24:31 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,10 @@
 void print_alphabet(void)
 {
 	char ch[100] = "I sometimes suffer from insomnia. And when I ";
-	char ch1[50] = "can't fall asleep, I play what I call the alphabet game";
+	char ch1[60] = "can't fall asleep, I play what I call the alphabet game";
 	int i = 0;
 
-	strncat(ch, ch1, 50);
+	strncat(ch, ch1, 60);
 
 	while (ch[i] != '\0')
 	{
