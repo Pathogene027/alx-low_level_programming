3494315c9ceb4b17992d1cd532b4361a05b22754
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-19 10:22:13 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* print_alphabet_x10 - it prints out alphabets 10 times
+*
+* Return: returns void
+*/
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		};
+		i++;
+	};
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,4 @@
 */
 int putchar(int n);
 void print_alphabet(void);
+void print_alphabet_x10(void);
