3504eb1a8ec09a887d39db075890fccccfd4a479
Pathogene027 <anthony.mensah027@gmail.com>
2022-10-20 16:00:13 -0700

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _isupper : checks for uppercases
+*
+* Return- returns 1 if c is upper or zero
+*/
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		c = 1;
+	};
+	else
+	{
+		c = 0;
+	};
+	return (c);
+}
